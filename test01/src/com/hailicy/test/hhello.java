5c1d1c62c098e35d641ae9f3fc8955a8e1e668e6
hailicy <2572729959@qq.com>
2020-03-21 17:39:22 +0800

    看看能不能放第二个项目文件夹

---
+++ test01/src/com/hailicy/test/hhello.java
@@ -0,0 +1,13 @@
+package com.hailicy.test;
+
+/**
+ * 创建时间: 2020/3/21 17:38
+ * 文件备注:
+ * 编写人员:
+ */
+
+public class hhello {
+    public static void main(String[] args) {
+        System.out.println("hello!");
+    }
+}
